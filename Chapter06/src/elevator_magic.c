2d139c37a178330a79bfbe3275965f9efb4c5674
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2018-04-28 20:13:53 -0600

    start work on C lib to support test interface

--- Chapter06/src/elevator_magic.c
+++ Chapter06/src/elevator_magic.c
@@ -1,3 +1,5 @@
+int input_codes[3] = {0, 0, 0};
+int error_code = 0;
 
 int is_override() {
    return 0;
@@ -12,12 +14,18 @@ int is_admin() {
 }
 
 void issue_override_code(int code) {
-   //C Code
+   input_codes[0] = 0;
+   input_codes[1] = 0;
+   input_codes[2] = 0;
+   input_codes[0] = code;
 }
 
 int poll_override_code() {
-   //C Code
-   return 0;
+   int code = input_codes[0];
+   input_codes[0] = input_codes[1];
+   input_codes[1] = input_codes[2];
+   input_codes[2] = 0;
+   return code;
 }
 
 int poll_override_input_floor() {
@@ -26,8 +34,7 @@ int poll_override_input_floor() {
 }
 
 int poll_override_error() {
-   //C Code
-   return 0;
+   return error_code;
 }
 
 void* poll_override_session() {
@@ -61,7 +68,10 @@ void override_normal_mode() {
 }
 
 void override_reset_state() {
-   //C Code
+   input_codes[0] = 0;
+   input_codes[1] = 0;
+   input_codes[2] = 0;
+   error_code = 0;
 }
 
 void elevator_display_flash(int pattern) {
