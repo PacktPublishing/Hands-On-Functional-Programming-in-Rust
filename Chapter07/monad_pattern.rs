dbdebebab2d842bb94b9efb8b4fea9d95a2d1cd4
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2018-05-06 22:22:38 -0600

    another monad

--- Chapter07/monad_pattern.rs
+++ Chapter07/monad_pattern.rs
@@ -1,3 +1,34 @@
+use std::fmt::{Debug};
+
+struct LogMonad<T>(T);
+impl<T> LogMonad<T> {
+   fn _return(t: T) -> LogMonad<T>
+      where T: Debug {
+      println!("{:?}", t);
+      LogMonad(t)
+   }
+   fn bind<R,F>(&self, f: F) -> LogMonad<R>
+      where F: Fn(&T) -> R,
+            R: Debug {
+      let r = f(&self.0);
+      println!("{:?}", r);
+      LogMonad(r)
+   }
+}
+
+struct LazyMonad<A,B>(Box<Fn(A) -> B>);
+impl<A: 'static,B: 'static> LazyMonad<A,B> {
+   fn _return(u: A) -> LazyMonad<B,B> {
+      LazyMonad(Box::new(move |b: B| b))
+   }
+   fn bind<C,G: 'static>(self, g: G) -> LazyMonad<A,C>
+      where G: Fn(B) -> C {
+      LazyMonad(Box::new(move |a: A| g(self.0(a))))
+   }
+   fn apply(self, a: A) -> B {
+      self.0(a)
+   }
+}
 
 fn main() 
 {
@@ -14,4 +45,17 @@ fn main()
    // does not permit polymorphic bind
    //let v4 = Some(2).or_else(|| Some("abc"));
    //println!("{:?}", v4);
+
+   LogMonad::_return(4)
+            .bind(|x| x+x)
+            .bind(|y| y*y)
+            .bind(|z| format!("{}{}{}", z, z, z));
+
+   let notyet = LazyMonad::_return(())
+                          .bind(|x| x+2)
+                          .bind(|y| y*3)
+                          .bind(|z| format!("{}{}", z, z));
+
+   let nowdoit = notyet.apply(222);
+   println!("nowdoit {}", nowdoit)
 }
