20738877433dbdc75aa26c99c1243cfff45508c8
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2018-05-07 20:09:38 -0600

    composition

--- Chapter07/combinator_pattern.rs
+++ Chapter07/combinator_pattern.rs
@@ -1,4 +1,17 @@
+fn compose<A,B,C,F,G>(f: F, g: G) -> impl Fn(A) -> C
+   where F: 'static + Fn(A) -> B,
+         G: 'static + Fn(B) -> C {
+   move |x| g(f(x))
+}
 
 fn main() 
 {
+   let fa = |x| x+1;
+   let fb = |y| y*2;
+   let fc = |z| z/3;
+
+   let g = compose(compose(fa,fb),fc);
+   println!("g(1) = {}", g(1));
+   println!("g(12) = {}", g(12));
+   println!("g(123) = {}", g(123));
 }
