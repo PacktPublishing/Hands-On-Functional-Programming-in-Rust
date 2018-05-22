1569c8d567a0bc23af96b5634120f1aebe06ec9f
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2018-05-22 12:55:07 -0600

    profiling

--- Chapter09/performance_profiling3.rs
+++ Chapter09/performance_profiling3.rs
@@ -1,2 +1,42 @@
+extern crate flame;
+use std::fs::File;
+use std::{thread,time};
+
+fn a() {
+   flame::start("fn a");
+   let t = time::Duration::from_millis(1000);
+   thread::sleep(t);
+   b();
+   b();
+   b();
+   flame::end("fn a");   
+}
+
+fn b() {
+   flame::start("fn b");
+   let t = time::Duration::from_millis(1000);
+   thread::sleep(t);
+   c();
+   c();
+   c();
+   flame::end("fn b");   
+}
+
+fn c() {
+   flame::start("fn c");
+   let t = time::Duration::from_millis(1000);
+   thread::sleep(t);
+   flame::end("fn c");
+}
+
 fn main() {
+   flame::start("fn main");
+   let t = time::Duration::from_millis(1000);
+   thread::sleep(t);
+   a();
+   a();
+   a();
+   flame::end("fn main");
+
+   flame::dump_html(&mut File::create("flame-graph.html").unwrap()).unwrap();
 }
